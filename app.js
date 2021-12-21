f77d4c6e5523f78b5d0033a2bbd817a1067fcf4c
nurjanahevs <evasitinurjanah2019@gmail.com>
2021-12-21 21:12:37 +0700

    changeurldatabase

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-const express = require('express');
-const router = require('./routers/routes')
-let cors = require('cors');
-const mongooseConnect = require('./config/server');
-const bodyParser = require('body-parser');
+const express = require("express");
+const router = require("./routers/routes");
+let cors = require("cors");
+const mongooseConnect = require("./config/server");
+const bodyParser = require("body-parser");
+const http = require("http");
 
-mongooseConnect()
-const app = express()
+mongooseConnect();
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
-app.use('/images', express.static('images'));
-app.use(bodyParser.json())
-app.use(router)
+app.use(cors());
+app.use("/images", express.static("images"));
+app.use(bodyParser.json());
+app.use(router);
 app.use(express.json());
 
 let port = process.env.PORT;
-    if(port == null || port == ''){
-    port = 3000
+if (port == null || port == "") {
+  port = 3000;
+}
 
-    }
+const server = http.createServer(app);
 
-app.listen(port, () => {
-    console.log(`App Run on http://localhost:${port}`)
-})
+server.listen(port, () => {
+  console.log(`App Run on port ${port}`);
+});
+
+// app.listen(port, () => {
+//     console.log(`App Run on http://localhost:${port}`)
+// })
